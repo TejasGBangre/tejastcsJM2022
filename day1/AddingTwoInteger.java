1bc0d132413e62342e3d71ec3da4f7caddc8e3cb
unknown <tejasgbangre@gmail.com>
2022-05-17 13:21:48 +0530

    day1

---
+++ day1/AddingTwoInteger.java
@@ -0,0 +1,18 @@
+package demo.jm;
+
+public class AddingTwoInteger {
+	
+	public static void main (String args[])
+	
+	{
+		
+		int first = 10;
+		int second = 23;
+		
+		int sum = first + second;
+		
+		System.out.println("The sum of two integer is " + sum);
+		
+	}
+	
+}
