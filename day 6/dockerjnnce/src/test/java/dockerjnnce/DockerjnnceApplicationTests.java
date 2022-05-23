69dcd7c0ee428d0fb24953005809cb41b5679aa7
unknown <tejasgbangre@gmail.com>
2022-05-23 18:46:58 +0530

    docker using springboot

---
+++ day 6/dockerjnnce/src/test/java/dockerjnnce/DockerjnnceApplicationTests.java
@@ -0,0 +1,13 @@
+package dockerjnnce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerjnnceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
